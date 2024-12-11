fdd4e8e1c5f0abeea5faaca58e4f677dd8f4a399
ahmerktk <u2024069@giki.edu.pk>
2024-12-11 19:06:04 +0500

    ahmer

--- project1.cpp
+++ project1.cpp
@@ -1,25 +1,75 @@
 #include<iostream>
-#include<cmath>
-using namespace std;
-<<<<<<< HEAD
-int main(){
-    cout<<"ibrahim";
-    cout<<"gabbar is back";
-    cout<<"hello budds";
-    return 0;
-}
-=======
+#include<stdlib.h>
+#include<conio.h>
 
-	int main(){
-	double base,exponent,result;
-		
-		cout<<"Enter a number:";
-		cin>>base;
-		
-		cout<<"Enter power:";
-		cin>>exponent;
+using namespace std;
+
+int days,amount,amount1;
+char driver;
+string name;
+int a = 0;
+string pass = "nigga";
+string new_pass;
+int op1;
+char x;
+int choice;
+
+void horil (string x)
+{
+	cout<<"-\t\t\t"<<x<<"\t\t\t-"<<endl;
+}
+void vertil ()
+{
+	cout<<"================================================================================="<<endl;
 	
-		
-		return 0;
+	void password()
+{
+	int i = 0;
+	while (i != 1)
+	{
+		cout<<"\t\t\t\tPLEASE ENTER THE PASSWORD: ";
+        cin>>new_pass;
+        if (new_pass == pass)
+		{
+			i = 1;
+		 } 
+		 else
+		 {
+		 	cout<<"Incorrect password"<<endl;
+		 	i = 0;
+		 }
 	}
->>>>>>> 1ab7fad810561998d94246f9805f78655b002896
+
+}
+}
+
+int main ()
+{
+	system("color f");
+	vertil();
+	horil("Welcome to Dabang Car Rental Service");
+	horil("Choose from the following:\t");
+	horil("1. ADMIN\t\t\t");
+	horil("2. CUSTOMER\t\t\t");
+	vertil();
+	cin>>op1;
+	switch (op1)
+	{
+		case 1:
+			password(); //need to add data
+			break;
+		case 2:
+			do
+    {
+    interface();
+    cout<<"\nDo you want to rent a car (y/n) ? : ";
+    cin>>x;
+    }
+    while (x!='y' && x!='Y');
+    CarsSelection();
+    cout<<"Which Car Do You Want ? : ";
+    cin>>choice;
+			break;
+	}
+	return 0;
+}
