cd5f5fc390e6d299c38a2070faaa59b70657d089
HassanSarfaraz <u2024401@giki.edu.pk>
2024-12-11 18:58:20 +0500

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -6,18 +6,3 @@ int main(){
     cout<<"hI try 2";
     return 0;
 }
-=======
-
-	int main(){
-	double base,exponent,result;
-		
-		cout<<"Enter a number:";
-		cin>>base;
-		
-		cout<<"Enter power:";
-		cin>>exponent;
-	
-		
-		return 0;
-	}
->>>>>>> 1ab7fad810561998d94246f9805f78655b002896
